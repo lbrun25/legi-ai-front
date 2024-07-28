5922bb2bacf45dee47db8c2f744c440af9d569c0
Lucien Brun <lucien.brun@epitech.eu>
2024-07-28 18:38:40 +0200

    Update searchArticles to support partitions

--- lib/supabase/searchArticles.ts
+++ lib/supabase/searchArticles.ts
@@ -13,15 +13,14 @@ export interface SearchMatchedArticlesResponse {
   articles: MatchedArticle[];
 }
 
-const fetchArticlesFromPartitions = async (maxIndex: number, embedding: number[], matchCount: number, codeTitle: string) => {
+const fetchArticles = async (embedding: number[], matchCount: number, codeTitle: string) => {
     try {
-      //console.log("Code appelé :",codeTitle)
-      console.time('call articles');
+      console.time(`call articles from ${codeTitle}`);
       const { data: matchedArticles, error } = await supabaseClient.rpc(`match_articles_${codeTitle}_adaptive`, {
         query_embedding: embedding,
         match_count: matchCount,
       });
-      console.timeEnd('call articles');
+      console.timeEnd(`call articles from ${codeTitle}`);
       if (error) {
         console.error(`Error in table ${codeTitle}`, error);
         return [];
@@ -33,21 +32,67 @@ const fetchArticlesFromPartitions = async (maxIndex: number, embedding: number[]
     }
 };
 
+const fetchArticlesFromPartitions = async (maxIndex: number, embedding: number[], matchCount: number, codeTitle: string) => {
+  const allArticles: MatchedArticle[] = [];
+  const promises: any[] = [];
+
+  for (let partitionIndex = 0; partitionIndex <= maxIndex; partitionIndex++) {
+    const promise = (async () => {
+      try {
+        console.time(`call articles from ${codeTitle} partition ${partitionIndex}`);
+        const { data: matchedArticles, error } = await supabaseClient.rpc(`match_articles_${codeTitle}_part_${partitionIndex}_adaptive`, {
+          query_embedding: embedding,
+          match_count: matchCount,
+        });
+
+        if (error) {
+          console.error(`Error fetching articles from ${codeTitle} partition ${partitionIndex}:`, error);
+          return [];
+        }
+
+        console.log(`Fetched articles from ${codeTitle} partition ${partitionIndex}:`, matchedArticles.map((m: MatchedArticle) => JSON.stringify({ number: m.number, similarity: m.similarity })));
+        console.timeEnd(`call articles from ${codeTitle} partition ${partitionIndex}`);
+        return matchedArticles;
+      } catch (err) {
+        console.error(`Exception occurred for ${codeTitle} partition ${partitionIndex}:`, err);
+        return [];
+      }
+    })();
+
+    promises.push(promise);
+  }
+
+  try {
+    const results = await Promise.allSettled(promises);
+    results.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        allArticles.push(...result.value);
+      } else {
+        if ("reason" in result) {
+          console.error('A promise was rejected:', result.reason);
+        }
+      }
+    });
+  } catch (err) {
+    console.error('Unexpected error occurred while fetching articles from partitions:', err);
+  }
+
+  return allArticles;
+}
+
 async function getCodeTitle(input: string): Promise<string> {
   const regex = /\[(.*?)\]/;
   const match = regex.exec(input);
 
   if (match && match[1]) {
-    const formattedString = match[1]
+    return match[1]
       .toLowerCase()
       .replace(/\s+/g, '_')
       .normalize("NFD")
       .replace(/[\u0300-\u036f]/g, '')
       .replace(/'/g, '');
-
-    return formattedString;
   } else {
-    console.log('Error no title of Code found in the query !');
+    console.warn('Error no title of Code found in the query.');
     return '';
   }
 }
@@ -77,8 +122,34 @@ export const searchMatchedArticles = async (input: string): Promise<SearchMatche
   const maxIndex = 4;
   const matchCount = 5;
 
+  const partitionedTablesByCodeTitle = [
+    "code_de_commerce",
+    "code_de_la_construction_et_de_lhabitation",
+    "code_monétaire_et_financier",
+    "code_de_procedure_penale",
+    "code_de_la_securite_sociale",
+    "code_du_travail"
+  ];
+  const isCodePartitioned = partitionedTablesByCodeTitle.includes(codeTitle);
+
+  if (isCodePartitioned) {
+    try {
+      const allArticles = await fetchArticlesFromPartitions(maxIndex, embedding, matchCount, codeTitle);
+      allArticles.sort((a, b) => b.similarity - a.similarity);
+      const topArticles = allArticles.slice(0, matchCount);
+      return {
+        articles: topArticles
+      };
+    } catch (error) {
+      console.error('Error occurred while fetching articles:', error);
+      return {
+        articles: []
+      };
+    }
+  }
+
   try {
-    const allArticles = await fetchArticlesFromPartitions(maxIndex, embedding, matchCount, codeTitle);
+    const allArticles = await fetchArticles(embedding, matchCount, codeTitle);
     const topArticles = allArticles.slice(0, matchCount);
     //console.log("got matched articles:", topArticles);
     return {
