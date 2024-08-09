f5eebffada4c1dba21a55aabd8277cedb59a63e9
Lucien Brun <lucien.brun@epitech.eu>
2024-08-09 18:41:39 +0200

    Fix parsing of marked decision

--- components/assistant.tsx
+++ components/assistant.tsx
@@ -161,7 +161,7 @@ export const Assistant = ({threadId: threadIdParams}: AssistantProps) => {
 
   // textCreated - create new assistant message
   const handleTextFormattingCreated = () => {
-    appendMessage("formatting", "");
+    appendMessage("assistant", "");
   };
 
   // textDelta - append text to last assistant message
