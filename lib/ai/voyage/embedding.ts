9d5b0e39fa289999cfae15a97ad191a2ab87c39f
Lucien Brun <lucien.brun@epitech.eu>
2024-09-03 23:03:42 +0200

    Fix VoyageAI rate limits

--- lib/ai/voyage/embedding.ts
+++ lib/ai/voyage/embedding.ts
@@ -51,3 +51,69 @@ export const embeddingWithVoyageLaw = async (input: string): Promise<VoyageEmbed
     return null;
   }
 };
+
+export const embeddingWithVoyageLawForDoctrines = async (input: string): Promise<VoyageEmbeddingResponse | null> => {
+  const url = 'https://api.voyageai.com/v1/embeddings';
+  const apiKey = process.env.VOYAGE_AI_API_KEY_FOR_DOCTRINES;
+  const headers = {
+    'Authorization': `Bearer ${apiKey}`,
+    'Content-Type': 'application/json',
+  };
+  console.log('embeddingWithVoyageLaw input:', input)
+  const body = {
+    input: input,
+    model: 'voyage-law-2'
+  };
+  const options = {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(body)
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const responseBody = await response.json();
+    if (!response.ok) {
+      console.error("Cannot embed text with Voyage AI:", (responseBody as VoyageEmbeddingErrorResponse).detail);
+      return null;
+    }
+    console.log('embeddingWithVoyageLaw res:', responseBody)
+    return responseBody;
+  } catch (error) {
+    console.error("Cannot embed text with Voyage AI: there was a problem with the fetch operation:", error);
+    return null;
+  }
+};
+
+export const embeddingWithVoyageLawForDecisions = async (input: string): Promise<VoyageEmbeddingResponse | null> => {
+  const url = 'https://api.voyageai.com/v1/embeddings';
+  const apiKey = process.env.VOYAGE_AI_API_KEY_FOR_DECISIONS;
+  const headers = {
+    'Authorization': `Bearer ${apiKey}`,
+    'Content-Type': 'application/json',
+  };
+  console.log('embeddingWithVoyageLaw input:', input)
+  const body = {
+    input: input,
+    model: 'voyage-law-2'
+  };
+  const options = {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(body)
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const responseBody = await response.json();
+    if (!response.ok) {
+      console.error("Cannot embed text with Voyage AI:", (responseBody as VoyageEmbeddingErrorResponse).detail);
+      return null;
+    }
+    console.log('embeddingWithVoyageLaw res:', responseBody)
+    return responseBody;
+  } catch (error) {
+    console.error("Cannot embed text with Voyage AI: there was a problem with the fetch operation:", error);
+    return null;
+  }
+};
