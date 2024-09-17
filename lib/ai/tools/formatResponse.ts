d8f91aca729f41867c40cacedf2979c31d981902
Lucien Brun <lucien.brun@epitech.eu>
2024-09-17 11:37:52 +0200

    fix build

--- lib/ai/tools/formatResponse.ts
+++
@@ -1,9 +0,0 @@
-import {tool} from "@langchain/core/tools";
-import {AssistantPrompt} from "@/lib/ai/langgraph/prompt";
-
-export const formatResponse = tool(async () => {
-  return AssistantPrompt;
-}, {
-  name: 'formatResponse',
-  description: "Cet outil doit être impérativement appelé pour générer la réponse finale destinée à l'utilisateur.",
-})
