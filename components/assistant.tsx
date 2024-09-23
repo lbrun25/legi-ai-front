82de202e35539d4abca2f9ba85fb7020ad6a5a8e
Lucien Brun <lucien.brun@epitech.eu>
2024-09-24 00:57:21 +0200

    Update explaining message

--- components/assistant.tsx
+++ components/assistant.tsx
@@ -40,7 +40,7 @@ export const Assistant = ({threadId: threadIdParams}: AssistantProps) => {
       setMessages([
         {
           role: "assistant",
-          text: `Comment puis-je vous aider aujourd’hui ?\n\nPour rappel, j’ai accès à des millions de sources juridiques à jour en temps réel et j'évolue tous les jours pour répondre à vos besoins. Plus vous m’utilisez et me fournissez de contexte, plus je peux vous offrir des réponses précises et pertinentes. Posez moi une question pour que je réalise une recherche juridique.`
+          text: `Bonjour Maître,\n\n**Comment puis-je vous aider aujourd’hui ?**\n\nPour rappel, j’ai accès à des **millions de sources juridiques** à jour en temps réel et j'évolue tous les jours pour répondre à vos besoins. Plus vous **m’utilisez et me fournissez de contexte**, plus je peux vous offrir des réponses précises et pertinentes. **Posez moi une question pour que je réalise une recherche juridique.**`
         }
       ])
     }
