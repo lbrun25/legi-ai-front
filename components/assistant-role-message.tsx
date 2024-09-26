e04807daa2234db27ec710dff505eb77effc9623
Lucien Brun <46843024+lbrun25@users.noreply.github.com>
2024-09-26 09:59:34 +0200

    Add background to role assistant message

--- components/assistant-role-message.tsx
+++ components/assistant-role-message.tsx
@@ -1,15 +1,15 @@
 export const AssistantRoleMessage = () => {
   return (
-    <div className="relative flex flex-row items-center">
-      <div
-        className={`absolute left-[-56px] w-[42px] h-[42px] rounded-full flex items-center justify-center z-20 bg-gradient-to-bl from-[#F0BAF3] to-[#FFBCBF] `}>
-        <span className="text-xl font-bold items-center justify-center text-center text-white font-montserrat">
-          {"m"}
-        </span>
-        <span className="text-3xl absolute top-[-1px] right-[5px] text-white font-montserrat">
-          {"."}
-        </span>
-      </div>
+    <div className="flex flex-row items-center space-x-3">
+      {/*<div*/}
+      {/*  className={`w-[42px] h-[42px] rounded-full flex items-center justify-center z-20 bg-gradient-to-bl from-[#F0BAF3] to-[#FFBCBF] `}>*/}
+      {/*  <span className="text-xl font-bold items-center justify-center text-center text-white font-montserrat">*/}
+      {/*    {"m"}*/}
+      {/*  </span>*/}
+      {/*  <span className="text-3xl absolute top-[-1px] right-[5px] text-white font-montserrat">*/}
+      {/*    {"."}*/}
+      {/*  </span>*/}
+      {/*</div>*/}
       <span className="text-xl font-bold">
         {"mike."}
       </span>
