c812ce81d3b12eedd89fbf2a8bf0f9e0c88df037
Romain Reffay <romainreffay@MacBook-Pro.lan>
2024-10-04 21:45:34 +0200

    update BM/Fusion + ajout reRanker

--- lib/ai/tools/getMatchedArticles.ts
+++ lib/ai/tools/getMatchedArticles.ts
@@ -6,7 +6,11 @@ export const getMatchedArticles = tool(async (input) => {
   if (!input.query) return "";
   const matchedArticlesResponse = await searchMatchedArticles(input.query);
   if (matchedArticlesResponse.hasTimedOut) return "";
-  return "#" + matchedArticlesResponse.articles?.map((article: MatchedArticle) => `Article ${article.number}: ${article.content}`).join("#");
+  const codeNameMatch = input.query.match(/\[([^\]]+)\]/);
+  const codeName = codeNameMatch ? codeNameMatch[1] : matchedArticlesResponse.codeName;
+  const formattedArticles = matchedArticlesResponse.articles?.map((article: MatchedArticle) => `\n• Article ${article.number} : "${article.content}"`).join("\n");
+  //console.log(`${codeName}:\n${formattedArticles}`)
+  return `${codeName}:\n${formattedArticles}`;
 }, {
   name: 'getMatchedArticles',
   description: "Obtient les articles les plus similaires basée sur la rêquete de l'utilisateur",
@@ -14,3 +18,14 @@ export const getMatchedArticles = tool(async (input) => {
     query: z.string().describe("Il est impératif de mentionner le nom du code entre crochets ([]) au début de la requête, suivi de termes juridiques précis et pertinents. Exemple : [Nom du code] terme1 terme2 ..."),
   })
 })
+
+export async function getMatchedArticles2(input: any) 
+{
+  if (!input) return "";
+  const matchedArticlesResponse = await searchMatchedArticles(input);
+  if (matchedArticlesResponse.hasTimedOut) return "";
+  const codeNameMatch = input.match(/\[([^\]]+)\]/);
+  const codeName = codeNameMatch ? codeNameMatch[1] : matchedArticlesResponse.codeName;
+  const formattedArticles = matchedArticlesResponse.articles?.map((article: MatchedArticle) => `\n• Article ${article.number} : "${article.content}"`).join("\n");
+  return `${codeName}:\n${formattedArticles}`; 
+}
