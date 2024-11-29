ecb1745055f4d7bc2472f1e561f3424988ce97af
Lucien Brun <lucien.brun@epitech.eu>
2024-11-29 10:34:49 +0100

    :sparkles: add waiting time below selected mode

--- components/select-mode.tsx
+++ components/select-mode.tsx
@@ -8,28 +8,46 @@ import {MikeMode} from "@/lib/types/mode";
 export const SelectMode = () => {
   const { selectedMode, setSelectedMode } = useAppState();
 
+  const getWaitingTimeStr = () => {
+    if (selectedMode === "research")
+      return "1 à 2 minutes";
+    if (selectedMode === "synthesis")
+      return "1 minute";
+    if (selectedMode === "analysis")
+      return "1 minute";
+    return "";
+  }
+
   return (
-    <SegmentedControl
-      name="select-mode"
-      onSelected={(mode) => setSelectedMode(mode as MikeMode)}
-      value={selectedMode}
-      segments={[
-        {
-          label: "Recherche juridique",
-          value: "research",
-          icon: <BotIcon className="w-4 h-4"/>
-        },
-        {
-          label: "Analyse",
-          value: "analysis",
-          icon: <FileSearchIcon className="w-4 h-4"/>
-        },
-        {
-          label: "Synthèse",
-          value: "synthesis",
-          icon: <FileIcon className="w-4 h-4"/>
-        },
-      ]}
-    />
+    <div className="space-y-4">
+      <SegmentedControl
+        name="select-mode"
+        onSelected={(mode) => setSelectedMode(mode as MikeMode)}
+        value={selectedMode}
+        segments={[
+          {
+            label: "Recherche juridique",
+            value: "research",
+            icon: <BotIcon className="w-4 h-4"/>
+          },
+          {
+            label: "Analyse",
+            value: "analysis",
+            icon: <FileSearchIcon className="w-4 h-4"/>
+          },
+          {
+            label: "Synthèse",
+            value: "synthesis",
+            icon: <FileIcon className="w-4 h-4"/>
+          },
+        ]}
+      />
+      <div className="flex items-center justify-center w-full">
+        <span
+          className="text-gray-500 text-sm font-medium text-center">
+          {`Temps d'attente estimé: ${getWaitingTimeStr()}`}
+        </span>
+      </div>
+    </div>
   );
 }
